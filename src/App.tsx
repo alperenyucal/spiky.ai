62c9e6ce811066186bd44434ce9567d3ab88597e
Alperen Yucal <alperenyucal@gmail.com>
2021-06-18 16:09:06 +0300

    feat: add timeline data visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
+import DataViz from "./components/DataViz/DataViz";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/about">"sa"</Route>
+        <Route path="/data-visualization">
+          <DataViz />
+        </Route>
       </Switch>
     </Router>
   );
